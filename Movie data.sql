cb5513bea214e1bbcb531b645c809f359ee48763
oggyluky11 <54610573+oggyluky11@users.noreply.github.com>
2019-09-08 05:03:01 -0400

    Add files via upload

---
+++ Movie data.sql
@@ -0,0 +1,45 @@
+create table if not exists movies (
+	Friend varchar(50),
+    Movie_1_Iron_Man_II int,
+    Movie_2_Captain_America_III int,
+    Movie_3_Doctor_Strange int,
+    Movie_4_Avengers_III int,
+    Movie_5_Avengers_VI int,
+    Movie_6_Black_Panther int)
+
+
+insert into movies values 
+('Silas',5,null,null,null,5,3),
+('Theodore',null,5,2,4,5,3),
+('Jack',5,5,3,null,5,null),
+('Finn',5,null,5,null,null,3),
+('Aarav',3,5,4,null,5,2),
+('Felix',null,null,4,5,4,null),
+('Henry',null,1,null,4,5,1),
+('Wyatt',null,4,null,4,5,null),
+('Aryan',5,2,null,3,5,null),
+('Oscar',2,2,3,5,5,5),
+('Oliver',null,2,null,4,null,2),
+('Leo',2,4,5,4,null,null),
+('Julian',1,4,null,null,null,4),
+('Levi',5,null,3,4,5,null),
+('Ethan',null,5,4,null,4,1),
+('Bodhi',5,null,2,5,4,5),
+('Arthur',2,5,3,4,5,null),
+('Louis',null,3,null,3,3,2),
+('James',2,3,1,4,5,null),
+('Ezra',1,2,3,3,4,null),
+('Soren',5,3,null,null,4,2),
+('Charles',4,5,3,4,4,1),
+('Jude',2,5,3,3,null,5),
+('Liam',3,null,1,2,5,null),
+('Aaron',2,1,4,4,null,3)
+
+select * from movies
+
+
+
+
+
+
+
